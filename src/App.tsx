ac8c0cf48d114ca88a2b0de0cbac1e2354e9294e
angelo choque <ancricoma_angelo@hotmail.com>
2024-10-15 03:13:18 -0500

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   return (
     <>
-      <div className="container mx-auto bg-red-950">
+      <div className="container mx-auto bg-red-950 ">
         <h1 className="text-2xl">Título Centrando</h1>
         <p>Este es un párrafo dentro del contenedor.</p>
+
       </div>
     </>
   );
