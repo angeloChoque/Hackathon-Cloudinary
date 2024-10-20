8a0e710e84c763bf95c20a5e6e4e4c26b3aa3007
Emma Mora <emmanu20021@gmail.com>
2024-10-20 00:04:24 -0600

    chore: Update homepage layout and styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,55 @@
-import Hero from '../components/hero';
+import Hero from "../components/hero";
 const Home = () => {
   return (
     <>
-      <Hero/>
-      <section className='hero'>
-
+      <Hero />
+      <section className="community-stories">
+        <div className="community-stories-body">
+          <h2 className="title">Historias de la comunidad</h2>
+          <div className="stories-container">
+            {stories.map((story) => (
+              <div
+                className="story-card"
+                style={{ "--banner-url": `url(${story.img})` } as React.CSSProperties}
+              >
+                <div className="card-header">
+                  <h3 className="card-title">{story.title}</h3>
+                  <p className="story-description">{story.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
     </>
   );
 };
+
+const stories = [
+  {
+    title: "Mi experiencia con Next.js",
+    description:
+      "Next.js es un framework de React que me ha permitido crear aplicaciones web de",
+    img: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  },
+  {
+    title: "Mi experiencia con Next.js",
+    description:
+      "Next.js es un framework de React que me ha permitido crear aplicaciones web de",
+    img: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  },
+  {
+    title: "Mi experiencia con Next.js",
+    description:
+      "Next.js es un framework de React que me ha permitido crear aplicaciones web de",
+    img: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  },
+  {
+    title: "Mi experiencia con Next.js",
+    description:
+      "Next.js es un framework de React que me ha permitido crear aplicaciones web de",
+    img: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  },
+];
 
 export default Home;
